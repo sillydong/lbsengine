182f2d8d403d0ad01dda2e728d8a27ea7d13b036
Chen.Zhidong <njutczd@gmail.com>
2017-10-21 12:50:27 +0800

    排序的数据

---
+++ types/scored_document.go
@@ -0,0 +1,22 @@
+package types
+
+//排序的struct
+type ScoredDocument struct{
+	Distance float64
+	DocId uint64
+}
+
+//排序
+type ScoredDocuments []*ScoredDocument
+
+func(docs ScoredDocuments)Len() int{
+	return len(docs)
+}
+
+func(docs ScoredDocuments)Swap(i,j int){
+	docs[i],docs[j] = docs[j],docs[i]
+}
+
+func(docs ScoredDocuments)Less(i,j int)bool{
+	return docs[i].Distance < docs[j].Distance
+}
